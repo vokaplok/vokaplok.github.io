959efec791744aa215e34d587ff5efe543d82e36
Andrii Kolpakov <41402825+vokaplok@users.noreply.github.com>
2020-01-13 06:03:58 +0200

    upd2.0

--- budget/js/app.js
+++ budget/js/app.js
@@ -23,6 +23,7 @@ class UI {
     this.budgetForm = document.getElementById("budget-form");
     this.budgetInput = document.getElementById("budget-input");
     this.budgetAmountInput = document.getElementById("budget-amount-input");
+	this.budgetPlan = document.getElementById("plan-budget-amount-input");
     this.budgetDateInput = document.getElementById("budget-date-input");
     //central main totals
     this.budgetAmount = document.getElementById("budget-amount");
@@ -33,6 +34,7 @@ class UI {
     this.expenseForm = document.getElementById("expense-form");
     this.expenseInput = document.getElementById("expense-input");
     this.expenseAmountInput = document.getElementById("expense-amount-input");
+	this.expensePlan = document.getElementById("plan-expense-amount-input");
     this.expenseDateInput = document.getElementById("expense-date-input");
     //lists and
     this.expenseList = document.getElementById("expense-list");
@@ -67,6 +69,7 @@ class UI {
   submitBudgetForm(){
     const budgetValue = this.budgetInput.value;
     const amountValue = this.budgetAmountInput.value;
+	const planBudgetValue = this.budgetPlan.value;
     const budgetDateValue = this.budgetDateInput.value;
     if(budgetValue === '' || amountValue === '' || amountValue < 0){
       this.budgetFeedback.classList.add('showItem');
@@ -78,13 +81,16 @@ class UI {
       }, 3000)
     } else {
       let amount = parseInt(amountValue);
+	  let plan = parseInt(planBudgetValue);
       this.budgetInput.value = '';
       this.budgetAmountInput.value = '';
+	  this.budgetPlan.value = '';
       this.budgetDateInput.value = '';
       let budget = {
         id: this.budgetitemID,
         title: budgetValue,
         amount: amount,
+		plan: plan,
         date: budgetDateValue
       }
       this.budgetitemID++;
@@ -125,6 +131,7 @@ class UI {
   submitExpenseForm(){
     const expenseValue = this.expenseInput.value;
     const amountValue = this.expenseAmountInput.value;
+	const planExpenseValue = this.expensePlan.value;
     const expenseDateValue = this.expenseDateInput.value;
     if(expenseValue === '' || amountValue === '' || amountValue < 0){
       this.expenseFeedback.classList.add('showItem');
@@ -137,11 +144,13 @@ class UI {
       let amount = parseInt(amountValue);
       this.expenseInput.value = '';
       this.expenseAmountInput.value = '';
+	  this.expensePlan.value = '';
       this.expenseDateInput.value = '';
       let expense = {
         id: this.itemID,
         title: expenseValue,
         amount: amount,
+		plan: plan,
         date: expenseDateValue
       }
       this.itemID++;
@@ -175,11 +184,80 @@ class UI {
     this.expenseAmount.textContent = total;
     return total;
   }
+  
+unique(arr) {
+  let result = [];
+
+  for (let str of arr) {
+    if (!result.includes(str)) {
+      result.push(str);
+    }
+  }
+
+  return result;
 }
 
+addRow(tableID){
+	//clear past table
+ 	var table = document.getElementById(tableID);
+	for(var i = table.rows.length - 1; i > 0; i--)
+	{
+		table.deleteRow(i);
+	}
+	
+	
+	let categories = [];
+	
+	let categorie = {
+		categ: "Cattest",
+		plan: "plantest",
+		fact: "amounttest",
+/* 		period: "error" */
+	}
+	
+
+  for (var i = 0; i < this.budgetitemList.length; i++) {
+		name = this.budgetitemList[i].title;
+		console.log(name);
+		
+	  if (categories.includes(this.budgetitemList[i].title)) {
+	  console.log("exist");
+	  Object.assign(this.budgetitemList[i].title, { plan: 3, fact: this.budgetitemList[i].amount, period: "success"});
+	  }
+	  
+	  else {
+		 console.log("not exist");
+		 
+		 name = Object.assign({}, categorie);
+		 Object.assign(name, { categ: this.budgetitemList[i].title, plan: 3, fact: this.budgetitemList[i].amount, period: "success"});
+		 
+	  }
+	  
+  }  
+  
+	//print
+   for (var i = 0; i < this.budgetitemList.length; i++) { 
+  		var table = document.getElementById(tableID);
+		var row = table.insertRow(-1);
+		var categoryCell = row.insertCell(0);
+		var planCell = row.insertCell(1);
+		var factCell = row.insertCell(2);
+		var minusCell = row.insertCell(3);
+/* 		var periodCell = row.insertCell(4);  */
+
+		categoryCell.innerHTML = this.budgetitemList[i].title;
+		planCell.innerHTML = this.budgetitemList[i].plan;
+		factCell.innerHTML = this.budgetitemList[i].amount;
+ 		minusCell.innerHTML = this.budgetitemList[i].plan - this.budgetitemList[i].amount; 
+/* 		periodCell.innerHTML = "yes"; */
+		categories.push(this.budgetitemList[i].title)
+  }
+}
+}
 function eventListeners(){
   const budgetForm = document.getElementById('budget-form');
   const expenseForm = document.getElementById('expense-form');
+  const categoryForm = document.getElementById('category-form');
   const sortBudget = document.getElementById('sort-form1');
   const sortExpense = document.getElementById('sort-form2');
 
@@ -203,6 +281,11 @@ function eventListeners(){
       console.log(sort.sortingByAmount());
       //CONSTRUCTOR PATTERN ALLOWS CREATE NEW OBJECTS IN CERTAIN TYPE
   })
+  
+  categoryForm.addEventListener('submit', function(event){
+    event.preventDefault();
+	ui.addRow('tabled');
+  })
 
   budgetForm.addEventListener('submit', function(event){
     event.preventDefault();
@@ -212,6 +295,7 @@ function eventListeners(){
   expenseForm.addEventListener('submit', function(event){
     event.preventDefault();
     ui.submitExpenseForm();
+
   })
 
 }
